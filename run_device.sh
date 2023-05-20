1f48e3a2b3ec7ab7ac4fd5b65cb585b9225aaf5b
Lorenzo De Santis <desantis.1849114@studenti.uniroma1.it>
2023-05-20 15:19:03 +0200

    updated all

--- run_device.sh
+++ run_device.sh
@@ -1,3 +1,3 @@
-docker run -d --ip 172.18.0.4 --name=traffic_cluster --net=influxdb --rm detector_simulator -t traffic -s 10 -i 1;
-docker run -d --ip 172.18.0.5 --name=weather_cluster --net=influxdb --rm detector_simulator -t weather -s 10 -i 2;
-docker run -d --ip 172.18.0.6 --name=energy_cluster --net=influxdb --rm detector_simulator -t energy -s 10 -i 3;
+# docker run -d --ip 172.18.0.4 --name=traffic_cluster --net=influxdb --rm detector_simulator -t traffic -s 10 -i 1 -f 10
+docker run -d --ip 172.18.0.5 --name=weather_cluster --net=influxdb --rm detector_simulator -t weather -s 10 -i 2 -f 10
+docker run -d --ip 172.18.0.6 --name=energy_cluster --net=influxdb --rm detector_simulator -t energy -s 10 -i 3 -f 10
