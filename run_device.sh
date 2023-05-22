7f9b61bd033fb5432d0abb6b0e7152cbd7364911
Lorenzo De Santis <desantis.1849114@studenti.uniroma1.it>
2023-05-22 18:42:23 +0200

    added queries and changed run script

--- run_device.sh
+++ run_device.sh
@@ -1,3 +1,4 @@
 docker run -d --ip 172.18.0.4 --name=traffic_cluster --net=influxdb --rm detector_simulator -t traffic -s 10 -i 1 -f 10 --seed 1
 docker run -d --ip 172.18.0.5 --name=weather_cluster --net=influxdb --rm detector_simulator -t weather -s 10 -i 2 -f 10 --seed 1
 docker run -d --ip 172.18.0.6 --name=energy_cluster --net=influxdb --rm detector_simulator -t energy -s 10 -i 3 -f 10 --seed 1
+docker run -d --ip 172.18.0.7 --name=notification_server --net=influxdb --rm notification_server
